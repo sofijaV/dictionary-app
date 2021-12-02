de33c98456ec30d292571e4cf93f0fa01bf9dee1
sofijaV <sofijavujaklija@yahoo.com>
2021-12-02 18:06:59 +0100

    added axios and dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 export default function Form() {
   let [word, setWord] = useState(null);
+  function showData(response) {
+    console.log(response.data[0]);
+  }
   function updateWord(event) {
     event.preventDefault();
     setWord(event.target.value);
   }
   function handleSubmit(event) {
     event.preventDefault();
-    return alert(word);
+    // documentation: https://dictionaryapi.dev/
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(url).then(showData);
   }
   return (
     <div className="Dictionary">
